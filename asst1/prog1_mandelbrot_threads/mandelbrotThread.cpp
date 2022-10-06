32da9363c5c6f7109964a656a4fbc6334d4dcce0
Yi Hu <yeehu@myth51.stanford.edu>
2022-10-05 21:51:30 -0700

    asst1/prog1

--- asst1/prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ asst1/prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -34,8 +34,25 @@ void workerThreadStart(WorkerArgs * const args) {
     // to compute a part of the output image.  For example, in a
     // program that uses two threads, thread 0 could compute the top
     // half of the image and thread 1 could compute the bottom half.
-
+    double startTime = CycleTimer::currentSeconds();
     printf("Hello world from thread %d\n", args->threadId);
+    
+    // int count;
+    // if (args->height % args->numThreads == 0) {
+    //     count = args->height / args->numThreads;
+    // } else {
+    //     count = args->height / args->numThreads + 1;
+    // };
+    // int startrow = args->threadId * count;
+    // int numrows = std::min(count,(int)args->height - startrow);
+    // mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width, args->height, startrow, numrows, args->maxIterations, args->output);
+    
+    for(int i= args->threadId; i< (int)args->height;i+=args->numThreads){
+        mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width, args->height, i, 1, args->maxIterations, args->output);
+    }
+    
+    double endTime = CycleTimer::currentSeconds();
+    printf("Thread %d finished in %.3f ms\n", args->threadId, (endTime - startTime) * 1000);
 }
 
 //
